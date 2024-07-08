2138546b211491b623aa5923120c6c636a6052d0
George Ofogba <ofogbageorge3@gmail.com>
2024-07-08 20:06:48 +0200

    first commit

---
+++ model/Academic/ClassLevel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const classLevelSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Admin',
+      required: true,
+    },
+    //students will be added to the class level when they register
+    students: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: student,
+      },
+    ],
+    //optional
+    subjects: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Subject',
+      },
+    ],
+    teachers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Teacher',
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const ClassLevel = mongoose.model('ClassLevel', classLevelSchema);
+
+module.exports = ClassLevel;
