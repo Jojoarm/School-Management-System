5b6d73e2e087707a39c94af8355a280182811013
George Ofogba <ofogbageorge3@gmail.com>
2024-07-09 19:48:11 +0200

    models created

---
+++ model/Academic/ExamResults.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+const examResultSchema = new mongoose.Schema(
+  {
+    student: {
+      type: Schema.Types.ObjectId,
+      ref: 'Student',
+      required: true,
+    },
+    exam: {
+      type: Schema.Types.ObjectId,
+      ref: 'Exam',
+      required: true,
+    },
+    grade: {
+      type: Number,
+      required: true,
+    },
+    score: {
+      type: Number,
+      required: true,
+    },
+    passMark: {
+      type: Number,
+      required: true,
+      default: 50,
+    },
+    //failed/passed
+    status: {
+      type: String,
+      required: true,
+      enum: ['failed', 'passed'],
+      default: 'failed',
+    },
+    //excellent/good/poor
+    remarks: {
+      type: String,
+      required: true,
+      enum: ['Excellent', 'Good', 'Poor'],
+      default: 'Poor',
+    },
+    position: {
+      type: Number,
+      required: true,
+    },
+    subject: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Subject',
+    },
+    classLevel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'ClassLevel',
+    },
+    academicTerm: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'AcademicTerm',
+      required: true,
+    },
+    academicYear: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'AcademicYear',
+      required: true,
+    },
+    isPublished: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//model
+const ExamResult = mongoose.model('ExamResult', examResultSchema);
+
+module.exports = ExamResult;
